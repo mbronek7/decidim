c55360549050dce37ba81a9ec159423b707535c3
Agustí B.R <agusti@codegram.com>
2020-04-16 08:17:38 +0200

    Add scopable helper methods and component specs

--- decidim-core/app/models/decidim/component.rb
+++ decidim-core/app/models/decidim/component.rb
@@ -9,6 +9,7 @@ module Decidim
     include Publicable
     include Traceable
     include Loggable
+    include Scopable
 
     belongs_to :participatory_space, polymorphic: true
 
@@ -85,6 +86,19 @@ module Decidim
       return false unless user
 
       participatory_space.can_participate?(user)
+    end
+
+    # Public: Returns the component Scope
+    # overrides the method from Scopable
+    def scope
+      return participatory_space.scope unless scopes_enabled
+
+      participatory_space.scopes.find_by(id: settings.scope_id)
+    end
+
+    # overrides the method from Scopable
+    def scopes_enabled
+      settings.try(:scopes_enabled)
     end
 
     delegate :serializes_specific_data?, to: :manifest
